41fe3d60d20715c674ad041aa1ee857775bdd2e1
Terrence Mahnken <terrencemm2@gmail.com>
2022-11-30 20:52:42 -0600

    feat: create layout component

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,47 +1,11 @@
-import { useState } from "react";
-import PropTypes from "prop-types";
-
-import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
 
-import itemData from "data/itemData";
-
-const Layout = (props) => {
-  const [items, setItems] = useState(itemData);
-  const [addedItems, setAddedItems] = useState([]);
-  const [availableItems, setAvailableItems] = useState(items);
-
+const Layout = ({ children }) => {
   return (
     <div>
-      <Container maxWidth="xl">
-        <Grid container>
-          <Grid item xs={12} sm={6}>
-            <Typography align="center" variant="h1">
-              Not Added
-            </Typography>
-            <Box>
-              {availableItems.map((item) => (
-                <>
-                  <Typography variant="body1">
-                    {item.icon} {item.itemName}
-                  </Typography>
-                </>
-              ))}
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Typography align="center" variant="h1">
-              Added
-            </Typography>
-          </Grid>
-        </Grid>
-      </Container>
+      <Container maxWidth="xl">{children}</Container>
     </div>
   );
 };
-
-Layout.propTypes = {};
 
 export default Layout;
