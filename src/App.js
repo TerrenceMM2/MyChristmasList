052aa5fb775408a610f1635f5083aebc263c3076
Terrence Mahnken <terrencemm2@gmail.com>
2022-11-30 20:53:40 -0600

    refactor: use Layout component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import Layout from "layouts/Layout";
 import { ThemeProvider } from "@mui/material";
 import customTheme from "styles/customTheme";
 
+import ListContainer from "components/ListContainer";
+
 function App() {
   return (
     <ThemeProvider theme={customTheme}>
-      <Layout />
+      <Layout>
+        <ListContainer />
+      </Layout>
     </ThemeProvider>
   );
 }
